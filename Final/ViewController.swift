d5d14ed16e7df75dd04bc394a1052e9aabf2828d
Isakov Sultanbek Aibekovich <isakov_s@auca.kg>
2020-12-18 08:08:56 +0600

    Initial Commit

---
+++ Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final
+//
+//  Created by Sultan Isakov on 12/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
